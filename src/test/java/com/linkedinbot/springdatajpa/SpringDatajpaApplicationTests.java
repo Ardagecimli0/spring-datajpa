780d8c2ca6345eb64e95ec0a22231d3889901198
Arda Gecimli <ardagecimli70@gmail.com>
2025-03-31 22:40:18 +0300

    Initial commit

---
+++ src/test/java/com/linkedinbot/springdatajpa/SpringDatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedinbot.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
