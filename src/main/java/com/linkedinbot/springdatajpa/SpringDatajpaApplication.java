f190628773c48ba1946e022829b8b34dec41dce6
Arda Gecimli <ardagecimli70@gmail.com>
2025-03-31 22:40:33 +0300

    Spring-data-jpa

--- src/main/java/com/linkedinbot/springdatajpa/SpringDatajpaApplication.java
+++ src/main/java/com/linkedinbot/springdatajpa/SpringDatajpaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDatajpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDatajpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDatajpaApplication.class, args);
+    }
 
 }
